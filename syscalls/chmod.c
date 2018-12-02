5f97cb17b862aca5f3fd71198ec67173fc1341a5
Yassine MHEDHBI <ymhedh@cs310.cs.mcgill.ca>
2018-12-02 18:09:27 -0500

    chmod test and clone both added and work

---
+++ syscalls/chmod.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv)
+{
+    
+    char buf[100] = "file.txt";
+    int i;
+    
+    chmod("tests.txt",0000007);
+    
+     
+        return(0);
+}
